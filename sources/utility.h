e6fc8b3edacf0ce62d0d159b98c743817e48b213
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-05-14 23:15:55 +0200

    Attempt at fixing build #11

--- sources/utility.h
+++ sources/utility.h
@@ -3,6 +3,8 @@
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#pragma once
+#include <string>
 #include <memory>
 #include <type_traits>
 #include <stdlib.h>
